bd348d2b36967e59767fe311cb2e3dc9aa64a5c0
KanesuSota <kanesusota@gmail.com>
2018-04-19 23:31:01 +0900

    練習

--- TableGenerator.py
+++ TableGenerator.py
@@ -4,5 +4,16 @@ Created on 2018/04/19
 @author: "KanesuSota"
 '''
 
-if __name__ == '__main__':
-    print("hello world")
+import xlrd
+
+book = xlrd.open_workbook("sample.xlsx")
+sheet = book.sheet_by_index(0)
+
+f = open("text.txt", "w")
+
+for col in range(sheet.ncols):
+    for row in range(sheet.nrows):
+        print(sheet.cell(row, col).value)
+
+f.write(sheet.cell(0,0).value)
+f.close()
